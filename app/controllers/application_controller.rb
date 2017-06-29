0424afdc54c7c20f7f2e7eaee01986e195d773be
adames <adameshh@gmail.com>
2017-06-29 13:18:46 -0400

    saving

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    @post = Post.create(params[:post])
+    redirect "/posts"
+  end
+
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get "/posts/:id/edit" do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch "/posts/:id" do
+    # binding.pry
+
+    @post = Post.find_or_create_by(params[:post])
+    @post.update(params[:post])
+
+    redirect "/posts/#{@post.id}"
+  end
+
 end
